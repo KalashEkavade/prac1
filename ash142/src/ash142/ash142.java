faac8f813f9025b93d6624b7d7acdab5c89583c3
Kalash <Kalash@LAPTOP-DUC4D5IC>
2023-05-03 00:58:43 +0530

    arcircle

---
+++ ash142/src/ash142/ash142.java
@@ -0,0 +1,13 @@
+package ash142;
+
+public class ash142 {
+	public static void main(String[] args) 
+	{
+		int r=7;
+		double pi=3.14,ar;
+		
+		System.out.println("The area of circle is");
+		ar = pi*r*r;
+		System.out.println(ar);
+	}
+}
