d885a1c146561410ef3b3fcda17e38c14fd4308d
KalashEkavade <97100620+KalashEkavade@users.noreply.github.com>
2023-05-03 11:16:58 +0530

    Update ash142.java

--- ash142/src/ash142/ash142.java
+++ ash142/src/ash142/ash142.java
@@ -13,3 +13,104 @@ public class ash142 {
 		\\im adding coment
 	}
 }
+package ash142;
+/**
+ * 
+ * @author Kalash
+ * @version (1)
+ * class creates a class for caterpillar
+ * <p>
+ * this class stimulates th working of caterpillar
+ * <br>
+ * which does two things
+ * <br>
+ * it moves and it eats
+ *
+ */
+
+public class Caterpillar {
+	private int moves;
+	private int cabbages_eaten;
+	/**
+	 * This is the constructor used to initialize attributes<br>
+	 * Eaten and moves
+	 * @param moves - the number of moves made by the caterpillar
+	 * @param eaten - the number of cabbages eaten by the caterpillar
+	 * 
+	 */
+	
+
+	public Caterpillar(int moves, int eaten)
+	{
+		this.setMoves(moves);
+		this.setCabbages_eaten(eaten);
+	}
+	
+	int getMoves() {
+		return moves;
+	}
+	/**
+	 * this the method to get the number of moves made by the caterpillar
+	 * <p>
+	 * @return- the number of moves
+	 */
+
+	void setMoves(int moves) {
+		this.moves = moves;
+	}
+	/**
+	 * this the method to set the number of moves made by the caterpillar
+	 * <p>
+	 * @return- the number of moves
+	 */
+
+	int getCabbages_eaten() {
+		return cabbages_eaten;
+	}
+	/**
+	 * this the method increments number of moves 
+	 * <p>
+	 * in particular to every move made the number of move is incremented by one
+	 * 
+	 */
+
+
+	void setCabbages_eaten(int cabbages_eaten) {
+		this.cabbages_eaten = cabbages_eaten;
+	}
+
+	void move()
+	{
+		setMoves(getMoves() + 1);
+	}
+
+	void eat()
+	{
+		this.setCabbages_eaten(this.getCabbages_eaten() + 1);
+	}
+	/**
+	 * this method displays the following,p>
+	 * 1)the number of moves made
+	 * 2)the number of cabbages eaten
+	 * 
+	 * @see get moves()
+	 * @see get cabbages eaten()
+	 */
+	
+	void display()
+	{
+		System.out.println("Moves made =" + getMoves());
+		System.out.println("Cabbages eaten =" + getCabbages_eaten());
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Caterpillar fred = new Caterpillar(0,0);
+		fred.move();
+		fred.eat();
+		fred.display();
+	}
+
+}
+
